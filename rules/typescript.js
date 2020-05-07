c9fc7c2b0087e8d7eb436233e70e03106c20adec
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-05-07 19:09:21 +0900

    allow __ prefixed unused variable

--- rules/typescript.js
+++ rules/typescript.js
@@ -4,8 +4,9 @@ module.exports = {
     ],
     "parser": "@typescript-eslint/parser",
     "rules": {
+        // allow prefix __ for exhaustive check
         "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/no-unused-vars": ["error", { "varsIgnorePattern": "^__" }],
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/restrict-template-expressions": ["error", { "allowNumber": true }],
     },
