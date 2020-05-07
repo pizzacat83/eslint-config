46bdea02e16c4186b4db1e7e199872d143598151
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-05-07 22:42:05 +0900

    fix naming conventions

--- rules/typescript.js
+++ rules/typescript.js
@@ -7,6 +7,21 @@ module.exports = {
         // allow prefix __ for exhaustive check
         "no-unused-vars": "off",
         "@typescript-eslint/no-unused-vars": ["error", { "varsIgnorePattern": "^__" }],
+
+        "camelcase": "off",
+        "@typescript-eslint/naming-convention": ["warn",
+            {
+                "selector": "default",
+                "format": ["camelCase"],
+                "leadingUnderscore": "allow",
+                "trailingUnderscore": "allow",
+            },
+            {
+                "selector": "typeLike",
+                "format": ["PascalCase"],
+            },
+        ],
+
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/restrict-template-expressions": ["error", { "allowNumber": true }],
     },
