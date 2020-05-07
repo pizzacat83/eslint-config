30b295212437bb4fe7a9ac9f9d23d739cc9a4027
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-05-07 22:42:29 +0900

    add extends for typescript*

--- typescript-react.js
+++ typescript-react.js
@@ -1,7 +1,9 @@
 module.exports = {
     "extends": [
         "airbnb",
+        "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "./rules/base.js",
         "./rules/react.js",
         "./rules/typescript.js",
--- typescript.js
+++ typescript.js
@@ -1,7 +1,9 @@
 module.exports = {
     "extends": [
         "airbnb-base",
+        "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "./rules/base.js",
         "./rules/typescript.js",
     ],
