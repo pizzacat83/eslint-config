aa4bc4547fcfb346431590bd546747603a005b96
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-05-17 14:09:19 +0900

    turn off no-return-await

--- rules/base.js
+++ rules/base.js
@@ -10,6 +10,8 @@ module.exports = {
         },
     },
     "rules": {
+        // Style
+
         "indent": ["warn", 4, { "SwitchCase": 1 }],
         "max-len": ["warn", 100],
         "import/extensions": "off",
@@ -33,5 +35,17 @@ module.exports = {
             "before": true,
             "after": true,
         }],
+
+        // Detecting Problems
+
+        // The await in the code below should not be removed, or the error won't be catched.
+        // try {
+        //     return await Promise.reject('fail');
+        // } catch (e) {}
+        // ESLint won't report this return-await as an issue :)
+        // but won't report the code without await either :(
+        // Having a habit of awaiting most of the promises prevents these cases,
+        // and I give priority to preventing errors over improving performance.
+        "no-return-await": "off",
     },
 };
