bdb62d5b1387d724890e592defe9397073f395b1
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-09-23 03:54:09 +0900

    add typescript support

--- rules/base.js
+++ rules/base.js
@@ -7,6 +7,7 @@ module.exports = {
             "node": {
                 "extensions": [".js", ".ts", ".jsx", ".tsx"],
             },
+            "typescript": {},
         },
     },
     "rules": {
