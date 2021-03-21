a74f0c80861206bac64a38d00474ca6283dfa8fc
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-03-21 21:54:32 +0900

    update typescript-eslint packages

--- rules/typescript.js
+++ rules/typescript.js
@@ -4,12 +4,11 @@ module.exports = {
   ],
   "parser": "@typescript-eslint/parser",
   "rules": {
+    // Style
+
     // allow prefix __ for exhaustive check
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": ["error", { "varsIgnorePattern": "^__" }],
-
-    "camelcase": "off",
-    "@typescript-eslint/camelcase": "off",
     "@typescript-eslint/naming-convention": ["warn",
       {
         "selector": "default",
@@ -27,11 +26,6 @@ module.exports = {
 
     "@typescript-eslint/member-delimiter-style": ["warn"],
 
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/restrict-template-expressions": ["error", { "allowNumber": true }],
-
-    "@typescript-eslint/no-floating-promises": ["error"],
-
     // Use const x: T = { hoge: 'fuga' }; instead of const x = { hoge: 'fuga' } as T;
     // tsc won't raise errors to the latter code, so eslint checks it.
     // I prefer obj as T to <T>obj because the latter looks like generics.
@@ -39,5 +33,22 @@ module.exports = {
       "assertionStyle": "as",
       "objectLiteralTypeAssertions": "never",
     }],
+
+    "@typescript-eslint/type-annotation-spacing": "warn",
+
+    "prefer-const": "error",
+
+
+    // Detecting Problems
+
+    "no-var": "error",
+
+    "prefer-spread": "error",
+
+    "prefer-rest-params": "error",
+
+    "@typescript-eslint/restrict-template-expressions": ["error", { "allowNumber": true }],
+
+    "@typescript-eslint/no-floating-promises": ["error"],
   },
 };
