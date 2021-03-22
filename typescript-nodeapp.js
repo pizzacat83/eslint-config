6dc7b82ce35284bc370d9e132721563df27e71e1
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-03-22 22:18:54 +0900

    create typescript-nodeapp preset

---
+++ typescript-nodeapp.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": [
+    "airbnb-base",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "./rules/base.js",
+    "./rules/nodeapp.js",
+    "./rules/typescript.js",
+  ],
+};
